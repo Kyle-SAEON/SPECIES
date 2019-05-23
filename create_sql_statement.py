9e3e9d7e67e70317a70fa911b1a04f883b5d0d75
kyle@saeon.ac.za <kylefcooper141290>
2019-05-23 14:37:55 +0200

    feat(init) create insert SQL for Species DB
    Takes a formatted CSV file and creates insert statements for SQL Server 2008

--- create_sql_statement.py
+++ create_sql_statement.py
@@ -2,19 +2,9 @@ import csv
 
 openfile = open('Species_0_To_be_added.csv', 'r')
 csvfile = csv.reader(openfile)
-
-#with open('Species_0_To_be_added.csv') as csvfile:
-#    csvReader = csv.reader(csvfile, delimiter=';')
 header = next(csvfile)
 headers = map((lambda x: '[' + x + ']'), header)
-#for row in csvReader:
-    #    print(row)
 insert = 'INSERT INTO [SPECIES_TEST].[dbo].[TblSpecies] (' + ", ".join(headers) + ") VALUES "
-#insert = "INSERT INTO [SPECIES_TEST].[dbo].[TblSpecies]([fTaxonomyID],[fLSID],[fWoRMID],[fSpeciesName]," \
-#     "[fCommonName],[fFeatures]," \
-#     "[fColour],[fSize],[fDistribution],[fSimilar],[fReferences],[fNotes],[fFAFFCode],[fHabitat]," \
-#     "[fFishBoard],[fBoxColor],[fGenus]," \
-#     "[fPhylum],[fClass],[fOrder],[fFamily],[fSpecies]) VALUES "
 for row in csvfile:
     values = map((lambda x: ''+x+''), row)
     print(insert +"("+ ", ".join(values) +") GO;" )
